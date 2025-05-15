5e74272ddf672705a0150acb6f04770a392e9bb7
ReacherDaniel <105395995+ReacherDaniel@users.noreply.github.com>
2025-05-14 23:38:07 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-require('graphql-import-node'); 
-const { ApolloServer } = require('apollo-server');
+require('dotenv').config();
+const fs = require('fs');
+const { ApolloServer, gql } = require('apollo-server');
 const resolvers = require('./src/resolvers/userResolvers');
-const typeDefs = require('./src/schema/user.graphql.js');
+
+
+const typeDefs = gql(fs.readFileSync('./src/schema/user.graphql', 'utf8'));
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: ({ req }) => ({ req })
 });
 
-server.listen({ port: 4000 }).then(({ url }) => console.log(`Servidor listo en ${url}`));
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`🚀 Servidor listo en ${url}`);
+});
